25de6cda43be93d5d8a605d8554c5cf568c78e57
Marouane Souiri <marwane.souiri.0@gmail.com>
2025-07-17 17:28:15 +0100

    feat: add Snowflake type with timestamp, worker, process, and sequence extraction

---
+++ snowflake.go
@@ -0,0 +1,73 @@
+/************************************************************************************
+ *
+ * yada (yet another discord api), A Lightweight Go library for Discord API
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ *
+ * Copyright 2025 Marouane Souiri
+ *
+ * Licensed under the BSD 3-Clause License.
+ * See the LICENSE file for details.
+ *
+ ************************************************************************************/
+
+package yada
+
+import (
+	"fmt"
+	"strconv"
+	"time"
+)
+
+/***********************
+ * Constants & Types   *
+ ***********************/
+
+// Discord epoch start time: January 1, 2015 UTC
+const discordEpoch = 1420070400000
+
+/***********************
+ *     Snowflake       *
+ ***********************/
+
+// Snowflake is a Discord unique identifier.
+type Snowflake uint64
+
+// String returns the Snowflake as string.
+func (s Snowflake) String() string {
+	return strconv.FormatUint(uint64(s), 10)
+}
+
+// Timestamp returns the creation time of the snowflake as time.Time.
+func (s Snowflake) Timestamp() time.Time {
+	ms := (uint64(s) >> 22) + discordEpoch
+	return time.UnixMilli(int64(ms))
+}
+
+// WorkerID extracts the internal Discord worker ID from the snowflake.
+func (s Snowflake) WorkerID() uint64 {
+	return (uint64(s) & 0x3E0000) >> 17
+}
+
+// ProcessID extracts the internal Discord process ID from the snowflake.
+func (s Snowflake) ProcessID() uint64 {
+	return (uint64(s) & 0x1F000) >> 12
+}
+
+// Sequence extracts the sequence number (increment part) of the snowflake.
+func (s Snowflake) Sequence() uint64 {
+	return uint64(s) & 0xFFF
+}
+
+/***********************
+ * Utilities           *
+ ***********************/
+
+// ParseSnowflake parses a string into a Snowflake.
+func ParseSnowflake(id string) (Snowflake, error) {
+	v, err := strconv.ParseUint(id, 10, 64)
+	if err != nil {
+		return 0, fmt.Errorf("invalid snowflake: %w", err)
+	}
+	return Snowflake(v), nil
+}
