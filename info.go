abbbc720bb6e9c08fa0348f3f1afd626da16f3ff
Marouane Souiri <marwane.souiri.0@gmail.com>
2025-07-17 17:27:57 +0100

    chore: add library info constants (name and version)

---
+++ info.go
@@ -0,0 +1,19 @@
+/************************************************************************************
+ *
+ * yada (yet another discord api), A Lightweight Go library for Discord API
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ *
+ * Copyright 2025 Marouane Souiri
+ *
+ * Licensed under the BSD 3-Clause License.
+ * See the LICENSE file for details.
+ *
+ ************************************************************************************/
+
+package yada
+
+const (
+	LIB_NAME    = "yada"
+	LIB_VERSION = "0.0.1"
+)
