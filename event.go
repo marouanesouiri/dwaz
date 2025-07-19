f27b5acb54f38686a95cf8f0246118c4a974e397
Marouane Souiri <marwane.souiri.0@gmail.com>
2025-07-19 02:30:05 +0100

    feat: add MessageCreateEvent and MessageDeleteEvent structs with JSON unmarshal logic

---
+++ event.go
@@ -0,0 +1,36 @@
+/************************************************************************************
+ *
+ * yada (yet another discord api), A Lightweight Go library for Discord API
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ *
+ * Copyright 2025 Marouane Souiri
+ *
+ * Licensed under the BSD 3-Clause License.
+ * See the LICENSE file for details.
+ *
+ ************************************************************************************/
+
+package yada
+
+import "github.com/bytedance/sonic"
+
+// MessageCreateEvent Message was created
+type MessageCreateEvent struct {
+	ShardsID int // shard that dispatched this event
+}
+
+func (e *MessageCreateEvent) fillFromJson(json []byte) error {
+	return sonic.Unmarshal(json, e)
+}
+
+// MessageDeleteEvent Message was deleted
+type MessageDeleteEvent struct {
+	ShardsID int // shard that dispatched this event
+}
+
+func (e *MessageDeleteEvent) fillFromJson(json []byte) error {
+	return sonic.Unmarshal(json, e)
+}
+
+// TODO: add other events
