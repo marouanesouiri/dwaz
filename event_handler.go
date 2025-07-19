e650cace61a85060f7480961fbd04dbd713ebfb5
Marouane Souiri <marwane.souiri.0@gmail.com>
2025-07-19 02:31:33 +0100

    feat: implement MESSAGE_CREATE and MESSAGE_DELETE handlers managers

---
+++ event_handler.go
@@ -0,0 +1,74 @@
+/************************************************************************************
+ *
+ * yada (yet another discord api), A Lightweight Go library for Discord API
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ *
+ * Copyright 2025 Marouane Souiri
+ *
+ * Licensed under the BSD 3-Clause License.
+ * See the LICENSE file for details.
+ *
+ ************************************************************************************/
+
+package yada
+
+/*****************************
+ *   MESSAGE_CREATE Handler
+ *****************************/
+
+// messageCreateHandlers manages all registered handlers for MESSAGE_CREATE events.
+type messageCreateHandlers struct {
+	logger   Logger
+	handlers []func(*MessageCreateEvent)
+}
+
+// handleEvent parses the MESSAGE_CREATE event data and calls each registered handler.
+func (h *messageCreateHandlers) handleEvent(shardID int, data []byte) {
+	evt := MessageCreateEvent{ShardsID: shardID}
+	if err := evt.fillFromJson(data); err != nil {
+		h.logger.Error("messageCreateHandlers: Failed parsing event data")
+		return
+	}
+
+	for _, handler := range h.handlers {
+		handler(&evt)
+	}
+}
+
+// addHandler registers a new MESSAGE_CREATE handler function.
+//
+// This method is not thread-safe.
+func (h *messageCreateHandlers) addHandler(handler any) {
+	h.handlers = append(h.handlers, handler.(func(*MessageCreateEvent)))
+}
+
+/*****************************
+ *   MESSAGE_DELETE Handler
+ *****************************/
+
+// messageDeleteHandlers manages all registered handlers for MESSAGE_DELETE events.
+type messageDeleteHandlers struct {
+	logger   Logger
+	handlers []func(*MessageDeleteEvent)
+}
+
+// handleEvent parses the MESSAGE_DELETE event data and calls each registered handler.
+func (h *messageDeleteHandlers) handleEvent(shardID int, data []byte) {
+	evt := MessageDeleteEvent{ShardsID: shardID}
+	if err := evt.fillFromJson(data); err != nil {
+		h.logger.Error("messageDeleteHandlers: Failed parsing event data")
+		return
+	}
+
+	for _, handler := range h.handlers {
+		handler(&evt)
+	}
+}
+
+// addHandler registers a new MESSAGE_DELETE handler function.
+//
+// This method is not thread-safe.
+func (h *messageDeleteHandlers) addHandler(handler any) {
+	h.handlers = append(h.handlers, handler.(func(*MessageDeleteEvent)))
+}
