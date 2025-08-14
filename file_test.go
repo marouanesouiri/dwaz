d316173ccbcbe021ea3f0e4e4e334b739f78250e
Marouane Souiri <marwane.souiri.0@gmail.com>
2025-08-14 10:56:59 +0100

    test(DownloadFile): add TestDownloadFile for DownloadFile function

--- file_test.go
+++ file_test.go
@@ -15,11 +15,19 @@ package yada
 
 import (
 	"encoding/base64"
+	"net/http"
+	"net/http/httptest"
 	"os"
 	"path/filepath"
 	"strings"
 	"testing"
 )
+
+// small valid PNG image (1x1 transparent pixel) (thanks to ChatGPT for that)
+// "iVBORw0K" is the Base64 representation of the PNG file signature (magic number)
+const base64Png = "iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR4nGNgYAAAAAMAAWgmWQ0AAAAASUVORK5CYII="
+
+// Helpers
 
 func decodeBase64(s string) []byte {
 	b, err := base64.StdEncoding.DecodeString(s)
@@ -29,10 +37,36 @@ func decodeBase64(s string) []byte {
 	return b
 }
 
+// Tests
+
+func TestDownloadFile(t *testing.T) {
+	imageData, _ := base64.StdEncoding.DecodeString(base64Png)
+
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "image/png")
+		_, _ = w.Write(imageData)
+	}))
+	defer server.Close()
+
+	tempDir := t.TempDir()
+	baseName := "testfile.jpg"
+
+	savedPath, err := DownloadFile(server.URL, baseName, tempDir)
+	if err != nil {
+		t.Fatalf("DownloadFile() error: %v", err)
+	}
+
+	wantPath := filepath.Join(tempDir, "testfile.png")
+	if savedPath != wantPath {
+		t.Errorf("DownloadFile() saved path = %q, want %q", savedPath, wantPath)
+	}
+
+	if _, err := os.Stat(savedPath); err != nil {
+		t.Fatalf("Expected file %s to exist, but got error: %v", savedPath, err)
+	}
+}
+
 func TestNewImageFile(t *testing.T) {
-	// small valid PNG image (1x1 transparent pixel) (thanks ChatGPT for that)
-	// "iVBORw0K" is the Base64 representation of the PNG file signature (magic number)
-	const base64Png = "iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR4nGNgYAAAAAMAAWgmWQ0AAAAASUVORK5CYII="
 
 	imgPath := filepath.Join(t.TempDir(), "test.png")
 	if err := os.WriteFile(imgPath, decodeBase64(base64Png), 0644); err != nil {
