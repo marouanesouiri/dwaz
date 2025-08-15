195570c772b5e529c436f650b1fc95093eaa83bd
Marouane Souiri <marwane.souiri.0@gmail.com>
2025-08-15 23:51:58 +0100

    test(permissions): add simple test for Permissions.Names
    - Verifies that Permissions.Names returns the correct names for a bitmask with multiple permissions.

---
+++ permissions_test.go
@@ -0,0 +1,32 @@
+/************************************************************************************
+ *
+ * yada (yet another discord api), A Lightweight Go library for Discord API
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ *
+ * Copyright 2025 Marouane Souiri
+ *
+ * Licensed under the BSD 3-Clause License.
+ * See the LICENSE file for details.
+ *
+ ************************************************************************************/
+
+package yada
+
+import "testing"
+
+func TestPermissionsNames(t *testing.T) {
+	perms := PermissionAddReactions | PermissionBanMembers
+
+	names := perms.Names()
+
+	if len(names) != 2 {
+		t.Errorf("Expeced the names slice len to be '2' got %d", len(names))
+	}
+
+	for _, name := range names {
+		if name != PermissionNameAddReactions && name != PermissionNameBanMembers {
+			t.Errorf("Expeced the name to be 'AddReactions' or 'BanMembers' go %s", name)
+		}
+	}
+}
