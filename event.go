2fd842dce1db322340db27131719cba0933ed990
Marwane Souiri <marwane.souiri.0@gmail.com>
2025-09-01 21:38:33 +0100

    feat: add MessageUpdateEvent and InteractionCreateEvent
    - Added `MessageUpdateEvent` struct with old and new messages.
    - Added `InteractionCreateEvent` with custom `UnmarshalJSON`.
    - Added `Message` field to `MessageCreateEvent` and `MessageDeleteEvent`.

--- event.go
+++ event.go
@@ -13,6 +13,8 @@
 
 package dwaz
 
+import "encoding/json"
+
 // ReadyCreateEvent Shard is ready
 type ReadyEvent struct {
 	ShardsID int // shard that dispatched this event
@@ -21,11 +23,37 @@ type ReadyEvent struct {
 // MessageCreateEvent Message was created
 type MessageCreateEvent struct {
 	ShardsID int // shard that dispatched this event
+	Message Message
+}
+
+// MessageCreateEvent Message was created
+type MessageUpdateEvent struct {
+	ShardsID   int // shard that dispatched this event
+	OldMessage Message
+	NewMessage Message
 }
 
 // MessageDeleteEvent Message was deleted
 type MessageDeleteEvent struct {
 	ShardsID int // shard that dispatched this event
+	Message Message
+}
+
+// InteractionCreateEvent Interaction created
+type InteractionCreateEvent struct {
+	ShardsID int // shard that dispatched this event
+	Interaction Interaction
+}
+
+var _ json.Unmarshaler = (*InteractionCreateEvent)(nil)
+
+// UnmarshalJSON implements json.Unmarshaler for InteractionCreateEvent.
+func (c *InteractionCreateEvent) UnmarshalJSON(buf []byte) error {
+	interaction, err := UnmarshalInteraction(buf)
+	if err == nil {
+		c.Interaction = interaction
+	}
+	return err
 }
 
 // TODO: add other events
