4fc870aa153d47e8878b636ee8a33904edeb9e82
Marwane Souiri <marwane.souiri.0@gmail.com>
2025-09-10 19:50:55 +0100

    refactor: remove custom UnmarshalJSON implementations

--- event.go
+++ event.go
@@ -13,28 +13,24 @@
 
 package dwaz
 
-import (
-	"encoding/json"
-
-	"github.com/bytedance/sonic"
-)
+import "encoding/json"
 
 // ReadyCreateEvent Shard is ready
 type ReadyEvent struct {
 	ShardsID int // shard that dispatched this event
+	Guilds   []Guild
+}
+
+// GuildCreateEvent Guild was created
+type GuildCreateEvent struct {
+	ShardsID int // shard that dispatched this event
+	Guild    GatewayGuild
 }
 
 // MessageCreateEvent Message was created
 type MessageCreateEvent struct {
 	ShardsID int // shard that dispatched this event
 	Message  Message
-}
-
-var _ json.Unmarshaler = (*MessageCreateEvent)(nil)
-
-// UnmarshalJSON implements json.Unmarshaler for MessageCreateEvent.
-func (e *MessageCreateEvent) UnmarshalJSON(buf []byte) error {
-	return sonic.Unmarshal(buf, &e.Message)
 }
 
 // MessageCreateEvent Message was created
@@ -44,24 +40,10 @@ type MessageUpdateEvent struct {
 	NewMessage Message
 }
 
-var _ json.Unmarshaler = (*MessageUpdateEvent)(nil)
-
-// UnmarshalJSON implements json.Unmarshaler for MessageCreateEvent.
-func (e *MessageUpdateEvent) UnmarshalJSON(buf []byte) error {
-	return sonic.Unmarshal(buf, &e.NewMessage)
-}
-
 // MessageDeleteEvent Message was deleted
 type MessageDeleteEvent struct {
 	ShardsID int // shard that dispatched this event
 	Message  Message
-}
-
-var _ json.Unmarshaler = (*MessageDeleteEvent)(nil)
-
-// UnmarshalJSON implements json.Unmarshaler for MessageDeleteEvent.
-func (e *MessageDeleteEvent) UnmarshalJSON(buf []byte) error {
-	return sonic.Unmarshal(buf, &e.Message)
 }
 
 // InteractionCreateEvent Interaction created
@@ -86,13 +68,6 @@ type VoiceStateUpdateEvent struct {
 	ShardsID int // shard that dispatched this event
 	OldState VoiceState
 	NewState VoiceState
-}
-
-var _ json.Unmarshaler = (*VoiceStateUpdateEvent)(nil)
-
-// UnmarshalJSON implements json.Unmarshaler for InteractionCreateEvent.
-func (c *VoiceStateUpdateEvent) UnmarshalJSON(buf []byte) error {
-	return sonic.Unmarshal(buf, c.NewState)
 }
 
 // TODO: add other events
